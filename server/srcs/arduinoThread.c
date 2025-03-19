9e521886905c98c8604f13db892ab8b6df60032e
yedididi <iyeji366@gmail.com>
2025-03-19 19:12:29 +0900

    arduino thread

--- server/srcs/arduinoThread.c
+++ server/srcs/arduinoThread.c
@@ -5,6 +5,7 @@ void    *arduinoThread(void *arg)
     t_data *arduinoData = (t_data *)arg;
     char buf[BUFSIZE];
 
+    //must add rfid here
     while (1)
     {
         int ret = read(arduinoData->clientfd, buf, BUFSIZE);
